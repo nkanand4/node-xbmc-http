ac6183bc0ec4516f79f91b36401649325e751182
Nitesh Kumar Anand <nitesh@Niteshs-MacBook-Pro.local>
2013-10-27 02:52:09 -0500

    First shot xbmc-node over http

---
+++ main.js
@@ -0,0 +1,27 @@
+var modules = ['Player', 'Input'];
+var instances = {};
+
+modules.forEach(function(module) {
+	var instance = require('./lib/'+module);
+	instances[module] = new instance;
+});
+
+var remote = {
+	invoke: function(command) {
+		var split = command.split('.');
+		var category = split[0];
+		var name = split[1];
+		var i, args = [];
+		for(i=1; i<arguments.length; i++) {
+			args.push(arguments[i]);
+		}
+		// example: invoking Player.invoke('PlayPause', arguments);
+		instances[category].invoke.apply(instances[category], [name].concat(args));
+	}
+};
+
+remote.invoke('Input.Left', function(data) {
+	if(typeof data.error === 'undefined') {
+		console.log('I can forward', data);
+	}
+});
